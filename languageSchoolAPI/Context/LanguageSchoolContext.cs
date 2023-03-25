81ec143508443ab417a83ecf32027286a10d5e9a
Edevaldo-Cruz <edevaldo_cruz@hotmail.com>
2023-03-25 10:24:46 -0300

    Criando conexão string

---
+++ languageSchoolAPI/Context/LanguageSchoolContext.cs
@@ -0,0 +1,19 @@
+﻿using languageSchoolAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace languageSchoolAPI.Context
+{
+    public class LanguageSchoolContext : DbContext
+    {
+        public LanguageSchoolContext(DbContextOptions<LanguageSchoolContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Teacher> Teacher { get; set; }
+        public DbSet<Classroom> Classrooms { get; set;}
+    }
+}
